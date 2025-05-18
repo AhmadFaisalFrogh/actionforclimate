4c443a3a3d469f54082c20cbea1ee96da2719383
AhmadFaisalFrogh <90568149+AhmadFaisalFrogh@users.noreply.github.com>
2025-05-17 23:12:04 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+
+  const hamburger = document.getElementById('hamburger');
+  const nav = document.getElementById('nav');
+
+  hamburger.addEventListener('click', () => {
+    nav.classList.toggle('show');
+  });
+
+  document.addEventListener("DOMContentLoaded", function () {
+    const navItems = document.querySelectorAll("nav ul li a");
+
+    navItems.forEach(item => {
+        item.addEventListener("click", function (event) {
+            event.preventDefault(); // Prevent default anchor behavior
+
+            const sectionId = this.getAttribute("href").substring(1);
+            const section = document.getElementById(sectionId);
+
+            if (section) {
+                section.scrollIntoView({ behavior: "smooth" });
+            }
+        });
+    });
+});
+
